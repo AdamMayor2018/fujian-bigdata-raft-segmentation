60f35ec8ff4f35b9876650569c27667944116462
caoxiang <caoxiang@yangshipin.cn>
2023-12-29 16:03:33 +0800

    首次提交 1.完成制作train/val数据集（overlap=0）

--- raft_baseline/util/data_builder.py
+++ raft_baseline/util/data_builder.py
@@ -77,7 +77,7 @@ def split_tiffs(tif_path: str, conf_loader: YamlConfigLoader):
     val_tif.save(os.path.join(par_dir, f"val_{name}"), "TIFF")
 
 
-class HuBMAPDataset(Dataset):
+class RaftTileDataset(Dataset):
     def __init__(self, filename, conf_loader: YamlConfigLoader):
         super().__init__()
         self.conf_loader = conf_loader
@@ -110,13 +110,14 @@ class HuBMAPDataset(Dataset):
         pad_right = self.pad_w - pad_left
         pad_top = self.pad_h // 2
         pad_bottom = self.pad_h - pad_top
-        self.image = F.pad(self.image, (pad_left, pad_right, pad_top, pad_bottom), mode='reflect').numpy().astype(np.uint8)
-        self.mask = F.pad(self.mask, (pad_left, pad_right, pad_top, pad_bottom), mode='reflect').numpy().astype(np.uint8)
+        self.image = F.pad(self.image, (pad_left, pad_right, pad_top, pad_bottom), mode='reflect').numpy().astype(np.uint8).transpose((1, 2, 0))
+        self.mask = F.pad(self.mask, (pad_left, pad_right, pad_top, pad_bottom), mode='reflect').numpy().astype(np.uint8).transpose((1, 2, 0))
 
         if self.h % self.sz < self.shift_h:
             self.num_h -= 1
         if self.w % self.sz < self.shift_w:
             self.num_w -= 1
+        self.new_h, self.new_w, _ = self.image.shape
 
     def __len__(self):
         return self.num_h * self.num_w
@@ -127,26 +128,22 @@ class HuBMAPDataset(Dataset):
         i_w = idx % self.num_w
         y = i_h * self.sz + self.shift_h
         x = i_w * self.sz + self.shift_w
-        py0, py1 = max(0, y), min(y + self.sz, self.h)
-        px0, px1 = max(0, x), min(x + self.sz, self.w)
+        py0, py1 = max(0, y), min(y + self.sz, self.new_h)
+        px0, px1 = max(0, x), min(x + self.sz, self.new_w)
 
         # placeholder for input tile (before resize)
         img_patch = np.zeros((self.sz, self.sz, 3), np.uint8)
         mask_patch = np.zeros((self.sz, self.sz, 1), np.uint8)
 
         # replace the value for img patch
-        if self.image.count == 3:
-            img_patch[0:py1 - py0, 0:px1 - px0] = \
-                np.moveaxis(self.image.read([1, 2, 3], window=Window.from_slices((py0, py1), (px0, px1))), 0, -1)
-        else:
-            for i, layer in enumerate(self.layers):
-                img_patch[0:py1 - py0, 0:px1 - px0, i] = \
-                    layer.read(1, window=Window.from_slices((py0, py1), (px0, px1)))
+        if self.image.shape[-1] == 3:
+            img_patch[0:py1 - py0, 0:px1 - px0] = self.image[py0: py1, px0: px1, :]
+                #np.moveaxis(self.image.read([1, 2, 3], window=Window.from_slices((py0, py1), (px0, px1))), 0, -1)
 
         # replace the value for mask patch
-        if self.mask.count == 1:
-            mask_patch[0:py1 - py0, 0:px1 - px0] = \
-                np.moveaxis(self.mask.read([1], window=Window.from_slices((py0, py1), (px0, px1))), 0, -1)
+        if self.mask.shape[-1] == 1:
+            mask_patch[0:py1 - py0, 0:px1 - px0] = self.mask[py0: py1, px0: px1, :]
+                #np.moveaxis(self.mask.read([1], window=Window.from_slices((py0, py1), (px0, px1))), 0, -1)
         return {'img': img_patch, 'mask': mask_patch}
 
 
@@ -161,19 +158,28 @@ if __name__ == '__main__':
     print(train_raw_img_paths)
     for j, i_path in enumerate(train_raw_img_paths):
         #split_tiffs(i_path, conf_loader)
-        dataset = HuBMAPDataset(i_path, conf_loader)
-        for i, pair in tqdm(enumerate(dataset)):
-            if i == 804:
-                print("xxx")
-            print(pair)
+        dataset = RaftTileDataset(i_path, conf_loader)
+        for i in range(len(dataset)):
+            print(f"making train ({j}_{i}) data pair.")
+            pair = dataset[i]
             image = pair["img"]
             mask = pair["mask"]
-            # 纯黑的图去掉
-            if mask.sum() == 0:
-                continue
-            print(image.shape, mask.shape)
+            #print(image.shape, mask.shape)
             image = Image.fromarray(image)
             mask = Image.fromarray(mask.squeeze(-1))
             image.save(opj(f"{train_dir}", "images", f"train_{j}_{i}.jpg"))
             mask.save(opj(f"{train_dir}", "labels", f"train_{j}_{i}.jpg"))
 
+    for j, i_path in enumerate(val_raw_img_paths):
+        #split_tiffs(i_path, conf_loader)
+        dataset = RaftTileDataset(i_path, conf_loader)
+        for i in range(len(dataset)):
+            print(f"making val ({j}_{i}) data pair.")
+            pair = dataset[i]
+            image = pair["img"]
+            mask = pair["mask"]
+            #print(image.shape, mask.shape)
+            image = Image.fromarray(image)
+            mask = Image.fromarray(mask.squeeze(-1))
+            image.save(opj(f"{val_dir}", "images", f"val_{j}_{i}.jpg"))
+            mask.save(opj(f"{val_dir}", "labels", f"val_{j}_{i}.jpg"))
