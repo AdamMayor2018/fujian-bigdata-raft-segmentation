79533c5bf4ca4bf68599c0410cabd58a9b82982b
caoxiang <caoxiang@yangshipin.cn>
2024-01-03 18:30:39 +0800

    首次提交 1.small fix 2.训练代码可以跑通

--- raft_baseline/train/train.py
+++ raft_baseline/train/train.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     model_params = conf_loader.attempt_load_param("model_params")
     model = build_model(model_name=conf_loader.attempt_load_param("backbone_name"), resolution=resolution,
                         deep_supervision=model_params["deep_supervision"], clf_head=model_params["clf_head"],
-                        clf_threshold=model_params["clf_threshold"], load_weights=model_params["load_backbone_weights"])
+                        clf_threshold=eval(model_params["clf_threshold"]), load_weights=model_params["load_backbone_weights"])
     # load pretrained
     if conf_loader.attempt_load_param("pretrained") and conf_loader.attempt_load_param("pretrained_path"):
         model.load_state_dict(torch.load(conf_loader.attempt_load_param("pretrained_path")))
@@ -85,57 +85,16 @@ if __name__ == '__main__':
         # FI score container
         prob_all = []
         target_all = []
-        for i, data in enumerate(train_epoch):
-            inputs = data[0]
-            targets = data[1]
-            #logger.info(f"start training batch : {i}.")
-            # print(data[0].shape, data[1].shape)
-            if model_params["clf_head"]:
-                y_clf = targets.to(device, torch.float32, non_blocking=True)
-                if model_params["deep_supervision"]:
-                    logits, logits_deeps, logits_clf = model(inputs.to(device, torch.float32, non_blocking=True))
-                else:
-                    logits, logits_clf = model(inputs.to(device, torch.float32, non_blocking=True))
-            else:
-                if model_params["deep_supervision"]:
-                    logits, logits_deeps = model(inputs.to(device, torch.float32, non_blocking=True))
-                else:
-                    logits = model(inputs.to(device, torch.float32, non_blocking=True))
-            y_true = targets.to(device, torch.float32, non_blocking=True)
-            #logger.info(f"{logits.shape}, {y_true.shape}")
-            logits = logits.squeeze(1)
-            loss = criterion(logits, y_true)
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-            train_loss += loss
-        #     target_all.extend(targets.flatten().tolist())
-        #     logits[logits >= 0.5] = 1
-        #     logits[logits <= 0.5] = 0
-        #     prob_all.extend(logits.flatten().tolist())
-        # logger.info(f"epoch {epoch}, train F1 score: {f1_score(target_all, prob_all)}")
-
-        avg_train_loss = train_loss / len(train_dataset)
-        logger.info(f"epoch {epoch}, train loss: {avg_train_loss}")
-        #validation
-        del data, loss, logits, y_true, inputs, targets
-        model.eval()
-        torch.cuda.empty_cache()
-        gc.collect()
-        loss_val = 0
-        y_preds = []
-        y_trues = []
-        val_epoch = tqdm(valid_loader, total=int(len(valid_loader)))
-        for i, data in enumerate(val_epoch):
-            inputs = data[0]
-            targets = data[1]
-            with torch.no_grad():
-                batch, c, h, w = inputs.shape
+        with torch.cuda.amp.autocast():
+            for i, data in enumerate(train_epoch):
+                inputs = data[0]
+                targets = data[1]
+                #logger.info(f"start training batch : {i}.")
+                # print(data[0].shape, data[1].shape)
                 if model_params["clf_head"]:
                     y_clf = targets.to(device, torch.float32, non_blocking=True)
                     if model_params["deep_supervision"]:
-                        logits, logits_deeps, logits_clf = model(
-                            inputs.to(device, torch.float32, non_blocking=True))
+                        logits, logits_deeps, logits_clf = model(inputs.to(device, torch.float32, non_blocking=True))
                     else:
                         logits, logits_clf = model(inputs.to(device, torch.float32, non_blocking=True))
                 else:
@@ -144,11 +103,53 @@ if __name__ == '__main__':
                     else:
                         logits = model(inputs.to(device, torch.float32, non_blocking=True))
                 y_true = targets.to(device, torch.float32, non_blocking=True)
-                loss_val += criterion(logits.squeeze(1), y_true).item() * batch
-            # release GPU memory cache
-            #del data, loss, logits, y_true, inputs, targets
+                #logger.info(f"{logits.shape}, {y_true.shape}")
+                logits = logits.squeeze(1)
+                loss = criterion(logits, y_true)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+                train_loss += loss
+            #     target_all.extend(targets.flatten().tolist())
+            #     logits[logits >= 0.5] = 1
+            #     logits[logits <= 0.5] = 0
+            #     prob_all.extend(logits.flatten().tolist())
+            # logger.info(f"epoch {epoch}, train F1 score: {f1_score(target_all, prob_all)}")
+
+            avg_train_loss = train_loss / len(train_dataset)
+            logger.info(f"epoch {epoch}, train loss: {avg_train_loss}")
+            #validation
+            del data, loss, logits, y_true, inputs, targets
+            model.eval()
             torch.cuda.empty_cache()
             gc.collect()
-        avg_val_loss = loss_val / len(valid_dataset)
-        scheduler.step(loss_val)
-        logger.info(f"epoch {epoch}, val loss: {avg_train_loss}")
+            loss_val = 0
+            y_preds = []
+            y_trues = []
+            val_epoch = tqdm(valid_loader, total=int(len(valid_loader)))
+            for i, data in enumerate(val_epoch):
+                inputs = data[0]
+                targets = data[1]
+                with torch.no_grad():
+                    batch, c, h, w = inputs.shape
+                    if model_params["clf_head"]:
+                        y_clf = targets.to(device, torch.float32, non_blocking=True)
+                        if model_params["deep_supervision"]:
+                            logits, logits_deeps, logits_clf = model(
+                                inputs.to(device, torch.float32, non_blocking=True))
+                        else:
+                            logits, logits_clf = model(inputs.to(device, torch.float32, non_blocking=True))
+                    else:
+                        if model_params["deep_supervision"]:
+                            logits, logits_deeps = model(inputs.to(device, torch.float32, non_blocking=True))
+                        else:
+                            logits = model(inputs.to(device, torch.float32, non_blocking=True))
+                    y_true = targets.to(device, torch.float32, non_blocking=True)
+                    loss_val += criterion(logits.squeeze(1), y_true).item() * batch
+                # release GPU memory cache
+                #del data, loss, logits, y_true, inputs, targets
+                torch.cuda.empty_cache()
+                gc.collect()
+            avg_val_loss = loss_val / len(valid_dataset)
+            scheduler.step(loss_val)
+            logger.info(f"epoch {epoch}, val loss: {avg_train_loss}")
