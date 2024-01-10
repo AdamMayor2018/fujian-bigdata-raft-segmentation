2c8fb53394fc56be30cd16c0d506dc13789aa8ec
caoxiang <adamcx@foxmail.com>
2024-01-10 22:14:20 +0800

    1.分开线上和线下的代码

--- raft_baseline/test.py
+++ raft_baseline/test.py
@@ -1,74 +1,101 @@
 # -- coding: utf-8 --
-# @Time : 2024/1/3 15:00
+# @Time : 2024/1/8 14:38
 # @Author : caoxiang
-# @File : test.py
+# @File : run.py.py
 # @Software: PyCharm
-# @Description:
-import cv2
-import segmentation_models_pytorch as smp
-import matplotlib.pyplot as plt
-from PIL import Image, ImageFile
-import torch
-import copy
-from raft_baseline.train.train import cal_np_f1_score
-#from raft_baseline.models.model import build_model
+# @Description: 按照官方要求提供的推理代码
+
 from raft_baseline.config.conf_loader import YamlConfigLoader
+import os, sys
+import cv2
+import torch
+import segmentation_models_pytorch as smp
+import numpy as np
+from PIL import Image, ImageFile
+from raft_baseline.train.dataset import RaftInferExpansionDataset
+from raft_baseline.train.transform import AugmentationTool
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+# ! 突破大文件限制, 读取4GB以上tif文件
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 Image.MAX_IMAGE_PIXELS = None
-# mask_path = "/data/cx/datasets/fujian_gis_data/train/labels/train_1_935.jpg"
-# mask = Image.open(mask_path)
-# mask = mask.convert("L")
-# plt.imshow(mask)
-# plt.show()
-from raft_baseline.train.transform import AugmentationTool
 
-conf_loader = YamlConfigLoader(yaml_path="config/raft_baseline_infer_config.yaml")
-device = conf_loader.attempt_load_param("device")
-device = device if torch.cuda.is_available() else "cpu"
-resolution = (conf_loader.attempt_load_param("train_width"), conf_loader.attempt_load_param("train_height"))
-# model_params = conf_loader.attempt_load_param("model_params")
-# model = build_model(model_name=conf_loader.attempt_load_param("backbone_name"), resolution=resolution,
-#                         deep_supervision=model_params["deep_supervision"], clf_head=model_params["clf_head"],
-#                         clf_threshold=eval(model_params["clf_threshold"]),
-#                         load_weights=model_params["load_backbone_weights"])
-# load pretrained
-model = smp.DeepLabV3Plus(
-    encoder_name='resnet18',
-    # encoder_weights='noisy-student',
-    in_channels=3,
-    classes=1,
-    activation=None
-)
-if conf_loader.attempt_load_param("pretrained") and conf_loader.attempt_load_param("pretrained_path"):
-    model.load_state_dict(torch.load(conf_loader.attempt_load_param("pretrained_path")))
-model = model.to(device)
-model.eval()
-transform = AugmentationTool(conf_loader).get_transforms_valid()
-image_path = "/data/cx/datasets/fujian_gis_data/val/images/val_0_471.jpg"
-mask_path = "/data/cx/datasets/fujian_gis_data/val/labels/val_0_471.jpg"
-image = cv2.imread(image_path)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-origin_image = copy.deepcopy(image)
-image = transform(image=image)["image"].unsqueeze(0)
-image = image.to(device)
-mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-mask[mask!=0] = 1
-print(image.shape)
-with torch.no_grad():
-    logits = model.predict(image)
-    logits = torch.sigmoid(logits)
-ratio = 0.5
-logits[logits >= ratio] = 1
-logits[logits < ratio] = 0
-logits = logits.squeeze(0).squeeze(0).cpu().detach().numpy()
-print(logits.shape)
-plt.figure()
-plt.title(f"test ratio: {ratio}")
-plt.subplot(1, 3, 1)
-plt.imshow(mask)
-plt.subplot(1, 3, 2)
-plt.imshow(logits)
-plt.subplot(1, 3, 3)
-plt.imshow(origin_image)
-plt.show()
-print(cal_np_f1_score(mask.flatten(), logits.flatten()))
+
+def cut_img(logits, result_mask, padding_size, matting_size, origin_indices):
+    if padding_size + matting_size > logits.shape[0]:
+        import pdb;pdb.set_trace()
+    try:
+        new_img = logits[padding_size:padding_size + matting_size, padding_size:padding_size + matting_size]
+        result_mask[origin_indices[1]: origin_indices[3], origin_indices[0]: origin_indices[2]] = new_img
+    except:
+        import pdb;pdb.set_trace()
+    return result_mask
+
+
+def main(to_pred_dir, result_save_path):
+    run_py_path = os.path.abspath(__file__)
+    model_dir = os.path.dirname(run_py_path)
+    conf_loader = YamlConfigLoader(yaml_path=os.path.join(model_dir, "config", "infer_config.yaml"))
+    ratio = conf_loader.attempt_load_param("ratio")
+    aug = AugmentationTool(conf_loader)
+    # model
+    model = smp.DeepLabV3Plus(
+        encoder_name=conf_loader.attempt_load_param("backbone"),
+        encoder_weights=None,
+        in_channels=3,
+        classes=1,
+        activation=None
+    )
+    if conf_loader.attempt_load_param("pretrained") and conf_loader.attempt_load_param("pretrained_path"):
+        model.load_state_dict(torch.load(os.path.join(model_dir, "weights", conf_loader.attempt_load_param("pretrained_path"))))
+    model = model.to("cuda:0")
+    model.eval()
+    # data
+    pred_imgs_paths = os.listdir(to_pred_dir)
+    pred_img_path = os.path.join(to_pred_dir, pred_imgs_paths[0])  # ! 测试集只有一张图片
+    image = Image.open(pred_img_path)
+    image = np.array(image)
+    height, width , _ = image.shape
+    result_mask = np.zeros((height, width), dtype=np.uint8)  # ! 结果mask
+    dataset = RaftInferExpansionDataset(file_path=pred_img_path, conf_loader=conf_loader, aug=aug)
+    with torch.no_grad():
+        for i in tqdm(range(len(dataset)), total=int(len(dataset))):
+            crop_image, pad_indices, origin_indices = dataset[i]
+            crop_image = crop_image.to("cuda:0")
+            crop_image = crop_image.unsqueeze(0)
+            logits = model.predict(crop_image)
+            logits = torch.sigmoid(logits)
+            logits[logits >= ratio] = 1
+            logits[logits < ratio] = 0
+            logits = logits.squeeze(0).squeeze(0).cpu().detach().numpy().astype(np.uint8)
+            cut_img(logits, result_mask, dataset.pad_size, dataset.matting_size, origin_indices)
+        # 腐蚀
+        #result_mask = cv2.morphologyEx(result_mask, cv2.MORPH_OPEN, kernel=(3, 3), iterations=3)
+        image_mask = Image.open(os.path.join(to_pred_dir, "..", 'val_mask1.tif'))
+        image_mask = np.array(image_mask)
+        image_mask[image_mask >= 1] = 1
+        TP = np.sum(np.logical_and(result_mask == 1, image_mask == 1))
+        FP = np.sum(np.logical_and(result_mask == 1, image_mask == 0))
+        FN = np.sum(np.logical_and(result_mask == 0, image_mask == 1))
+        precision = TP / (TP + FP)
+        recall = TP / (TP + FN)
+        f1 = 2 * precision * recall / (precision + recall)
+        print(f1)
+        plt.subplot(1, 2, 1)
+        plt.imshow(result_mask)
+        plt.subplot(1, 2, 2)
+        plt.imshow(image_mask)
+        plt.show()
+
+
+    #! PIL保存
+    # pred = Image.fromarray(result_mask)
+    # pred.save(result_save_path)
+
+
+if __name__ == "__main__":
+    # to_pred_dir = sys.argv[1]  # 所需预测的文件夹路径
+    # result_save_path = sys.argv[2]  # 预测结果保存文件路径
+    to_pred_dir = "/data/cx/datasets/fujian_gis_data/pred"
+    result_save_path = "/data/user/zhaozeming/competition/result.tif"
+    main(to_pred_dir, result_save_path)
--- raft_baseline/train/dataset.py
+++ raft_baseline/train/dataset.py
@@ -9,6 +9,11 @@ from raft_baseline.config.conf_loader import YamlConfigLoader
 from glob import glob
 from model.transform import AugmentationTool
 import pandas as pd
+from PIL import Image, ImageFile
+import torch
+import torch.nn.functional as F
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+Image.MAX_IMAGE_PIXELS = None
 
 
 class BucketedDataset(Dataset):
@@ -219,6 +224,69 @@ class RaftDataset(Dataset):
         return len(self.pairs["images"])
 
 
+class RaftInferExpansionDataset(Dataset):
+    def __init__(self, file_path, conf_loader: YamlConfigLoader, aug: AugmentationTool):
+        self.image = np.array(Image.open(file_path)).astype(np.float32).transpose((2, 0, 1))
+        self.image = torch.from_numpy(self.image)
+        self.transform = aug.get_transforms_valid()
+        # print(self.image.shape)
+        _, self.height, self.width = self.image.shape
+        self.tile_size = conf_loader.attempt_load_param("tile_size")
+        self.pad_size = conf_loader.attempt_load_param("pad_size")
+        self.matting_size = conf_loader.attempt_load_param("tile_size") - 2 * self.pad_size
+        # 先计算在原图上下左右都按照expansion尺寸pad一圈之后再进行滑窗
+        # pad image
+        self.pad_image = F.pad(self.image, (self.pad_size, self.pad_size, self.pad_size, self.pad_size),
+                           mode='reflect').numpy().astype(np.uint8)
+        # self.result_blank = np.ones((2000 - 2*self.pad_size, 2000 - 2*self.pad_size, 3)).astype(np.uint8) * 255
+        # self.height, self.width, _ = self.result_blank.shape
+        # self.pad_image = np.ones((2000, 2000, 3)).astype(np.uint8)
+        # self.blank = np.ones_like(self.pad_image).astype(np.uint8) * 255
+        #_, self.pad_height, self.pad_width = self.image.shape
+        _, self.pad_height, self.pad_width= self.pad_image.shape
+        self.num_h = self.height // self.matting_size  # 横着有多少块
+        self.num_w = self.width // self.matting_size # 竖着有多少块
+        self.num_h += 1 if (self.pad_height % self.tile_size) != 0 else self.num_h
+        self.num_w += 1 if (self.pad_width % self.tile_size) != 0 else self.num_w
+
+    def __len__(self):
+        return self.num_w * self.num_h
+
+    def __getitem__(self, idx):
+        #返回的应该是pad之后的切片图像（给到模型）和在原始图mask上的填充索引
+        i_h = idx // self.num_w
+        i_w = idx % self.num_w
+
+        # pad crop 左上角顶点
+        pad_ymin = i_h * self.tile_size if i_h == 0 else i_h * self.tile_size - 2 * self.pad_size * i_h
+        pad_xmin = i_w * self.tile_size if i_w == 0 else i_w * self.tile_size - 2 * self.pad_size * i_w
+
+        # pad crop idx
+        pad_ymax = min(pad_ymin + self.tile_size, self.pad_height)
+
+        pad_xmax = min(pad_xmin + self.tile_size, self.pad_width)
+        pad_xmin = pad_xmin if pad_xmin + self.tile_size < self.pad_width else self.pad_width - self.tile_size
+        pad_ymin = pad_ymin if pad_ymin + self.tile_size < self.pad_height else self.pad_height - self.tile_size
+        # self.blank = draw_box(self.blank, cords=(pad_xmin, pad_ymin, pad_xmax, pad_ymax), color=(255, 0, 0), thickness=3)
+
+        # 切片图像
+        crop_image = self.pad_image[:, pad_ymin: pad_ymax, pad_xmin: pad_xmax]
+        crop_image = crop_image.transpose((1, 2, 0))
+
+        #计算里面去除pad后小框的位置
+        orgin_ymin = i_h * self.matting_size
+        orgin_xmin = i_w * self.matting_size
+        orgin_xmax = min(orgin_xmin + self.matting_size, self.width)
+        orgin_ymax = min(orgin_ymin + self.matting_size, self.height)
+        orgin_xmin = orgin_xmin if orgin_xmin + self.matting_size < self.width else self.width - self.matting_size
+        orgin_ymin = orgin_ymin if orgin_ymin + self.matting_size < self.height else self.height - self.matting_size
+        # self.result_blank = draw_box(self.result_blank, cords=(orgin_xmin, orgin_ymin, orgin_xmax, orgin_ymax), color=(0, 255, 0),
+        #                       thickness=3)
+        # origin crop idx
+        if self.transform:
+            crop_image = self.transform(image=crop_image)["image"]
+
+        return crop_image,  [pad_xmin, pad_ymin, pad_xmax, pad_ymax], [orgin_xmin, orgin_ymin, orgin_xmax, orgin_ymax]
 if __name__ == '__main__':
 
     conf_loader = YamlConfigLoader(yaml_path="../config/raft_baseline_config.yaml")
